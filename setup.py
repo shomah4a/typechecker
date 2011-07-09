a1404dc3dde59ff42b5ce9d0a60689433195e1b7
shoma <shoma.h4a+github@gmail.com>
2011-07-09 12:36:35 +0900

    add metadata for PyPI registration

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 #-*- coding:utf-8 -*-
 
-
 import setuptools
+import typechecker
 
 version = '0.1.0'
 
 setuptools.setup(
     name='typechecker',
+    version=typechecker.__version__,
     packages=['typechecker'],
     install_requires=[
         ],
-    version=version)
-
+    author=typechecker.__author__,
+    author_email='shoma.h4a+pypi@gmail.com',
+    license=typechecker.__license__,
+    url='https://github.com/shomah4a/typechecker',
+    description='This module provides dynamic type check mechanism.',
+    long_description=typechecker.__doc__,
+    classifiers='''
+Programming Language :: Python
+Development Status :: 2 - Pre-Alpha
+License :: OSI Approved :: GNU General Public License (GPL)
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: Utilities
+'''.strip().splitlines())
--- typechecker/__init__.py
+++ typechecker/__init__.py
@@ -1,1 +1,19 @@
 #-*- coding:utf-8 -*-
+'''
+This module provides dynamic type check mechanism.
+
+  >>> from typechecker.typeinfo import typeinfo
+  >>> @typeinfo(int, x=int, y=int)
+  ... def add(x, y):
+  ...     return x + y
+  >>> add(10, 20)
+  30
+  >>> add('aa', 'bb')
+  TypeError
+
+'''
+
+__version__ = '0.1.0'
+__author__ = 'Shoma Hosaka'
+__license__ ='GPL'
+
